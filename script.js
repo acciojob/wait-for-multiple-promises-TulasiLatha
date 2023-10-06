c2efaefddf3a93981cbdb459a7b7d349e44a5aa7
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-10-06 22:23:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,76 @@
-const table = document.getElementById("table");
-
-function createPromise() {
-  const randomTime = Math.floor(Math.random() * 3000) + 1000;
+// Function to create a promise with a random delay between 1 and 3 seconds
+function createPromise(id) {
+  const delay = Math.floor(Math.random() * 3000) + 1000; // Random delay between 1 and 3 seconds
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(randomTime);
-    }, randomTime);
+      resolve({
+        id: `Promise ${id}`,
+        delay: delay / 1000, // Convert milliseconds to seconds
+      });
+    }, delay);
   });
 }
 
-const promises = [createPromise(), createPromise(), createPromise()];
+// Function to update the table with promise results
+function updateTableWithPromises() {
+  // Select the table body
+  const tableBody = document.getElementById("output");
 
-const loadingRow = table.insertRow();
-const loadingCell = loadingRow.insertCell();
-loadingCell.colSpan = 2;
-loadingCell.textContent = "Loading...";
+  // Create a row for loading text
+  const loadingRow = document.createElement("tr");
+  const loadingCol = document.createElement("td");
+  loadingCol.textContent = "Loading...";
+  loadingCol.colSpan = 2; // Span two columns
+  loadingRow.appendChild(loadingCol);
+  tableBody.appendChild(loadingRow);
 
-Promise.all(promises)
-  .then((results) => {
-    table.deleteRow(0);
+  // Create an array to store the promises
+  const promises = [];
 
-    const row1 = table.insertRow();
-    row1.insertCell().textContent = "Promise 1";
-    row1.insertCell().textContent = `${(results[0] / 1000).toFixed(3)}`;
+  // Create three promises and add them to the array
+  for (let i = 1; i <= 3; i++) {
+    promises.push(createPromise(i));
+  }
 
-    const row2 = table.insertRow();
-    row2.insertCell().textContent = "Promise 2";
-    row2.insertCell().textContent = `${(results[1] / 1000).toFixed(3)}`;
+  // Wait for all promises to resolve
+  Promise.all(promises)
+    .then((results) => {
+      // Remove the loading row
+      loadingRow.remove();
 
-    const row3 = table.insertRow();
-    row3.insertCell().textContent = "Promise 3";
-    row3.insertCell().textContent = `${(results[2] / 1000).toFixed(3)}`;
+      // Create rows for each promise result
+      results.forEach((result) => {
+        const newRow = document.createElement("tr");
+        const col1 = document.createElement("td");
+        const col2 = document.createElement("td");
 
-    const totalTime = results.reduce((acc, time) => acc + time, 0);
-    const totalRow = table.insertRow();
-    totalRow.insertCell().textContent = "Total";
-    totalRow.insertCell().textContent = `${(totalTime / 1000).toFixed(3)}`;
-  })
-  .catch((error) => console.error(error));
+        col1.textContent = result.id;
+        col2.textContent = result.delay.toFixed(3); // Format delay as a decimal number with 3 decimal places
+
+        newRow.appendChild(col1);
+        newRow.appendChild(col2);
+
+        tableBody.appendChild(newRow);
+      });
+
+      // Calculate and add the total row
+      const totalRow = document.createElement("tr");
+      const totalCol1 = document.createElement("td");
+      const totalCol2 = document.createElement("td");
+
+      totalCol1.textContent = "Total";
+      const totalTime = results.reduce((total, result) => total + result.delay, 0);
+      totalCol2.textContent = totalTime.toFixed(3); // Format total time as a decimal number with 3 decimal places
+
+      totalRow.appendChild(totalCol1);
+      totalRow.appendChild(totalCol2);
+
+      tableBody.appendChild(totalRow);
+    })
+    .catch((error) => {
+      console.error("At least one promise rejected:", error);
+    });
+}
+
+// Call the function to update the table with promises
+updateTableWithPromises();
